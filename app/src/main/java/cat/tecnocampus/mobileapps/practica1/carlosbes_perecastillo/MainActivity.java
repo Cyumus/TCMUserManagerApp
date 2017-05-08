e6e091d0c519b0f0af62049c92fd584650330189
Cyumus <carlosbg1995@gmail.com>
2017-05-08 21:59:02 +0200

    + Main Activity code cleaned and commented

--- app/src/main/java/cat/tecnocampus/mobileapps/practica1/carlosbes_perecastillo/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica1/carlosbes_perecastillo/MainActivity.java
@@ -18,24 +18,48 @@ import cat.tecnocampus.mobileapps.practica1.carlosbes_perecastillo.adapters.DbAd
 /**
  * Created by Carlos Bes on 05/05/2017.
  */
-// todo comment
-// todo clean code
+
+/**
+ * This is the Main Activity of the Application
+ */
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Class constants
+     */
     private static final int CREATE_STUDENT = 0;
     private static final int EDIT_STUDENT = 1;
 
+    /**
+     * Main Activity Attributes
+     * - mBtnCreate. The Floating Action Button on the bottom-right corner of the view. It creates
+     * new Students.
+     * - mMenu. The Toolbar of the Application. It has the edit and delete options when some
+     * items are selected.
+     * - mStudentListFragment. The visual and logic representation of the Student list as a Fragment
+     */
     FloatingActionButton mBtnCreate;
     Toolbar mMenu;
-    private FragmentManager mFragmentManager;
     private StudentListFragment mStudentListFragment;
 
 
+    /**
+     * This function is called when the Main Activity is created.
+     * First, it gets the current Fragment Manager to manage the future transactions.
+     * Then, it gets the Floating Action Button btnCreate and sets its click listener to start the
+     * activity UserEdit and waits for a result.
+     *
+     * Sets the Toolbar mMenu as the Action Bar of the Application.
+     *
+     * And begins the transaction of the mStudentListFragment in order to add it to the current view
+     * by replacing it.
+     * @param savedInstanceState The saved instance state before destroying the old activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         // Create Student Floating Action Button
         mBtnCreate = (FloatingActionButton) findViewById(R.id.createStudentButton);
@@ -51,12 +75,25 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mMenu);
 
         // Adding Student List Recycled View
-        FragmentTransaction addStudentListTransaction = mFragmentManager.beginTransaction();
+        FragmentTransaction addStudentListTransaction = fragmentManager.beginTransaction();
         mStudentListFragment = StudentListFragment.newInstance();
         addStudentListTransaction.replace(R.id.student_list, mStudentListFragment);
         addStudentListTransaction.commit();
     }
 
+    /**
+     * This function is called whe the Activity was waiting for a result and actually it gets it.
+     *
+     * If the Main Activity was waiting for a CREATE_STUDENT result, it will create the Student
+     * using the received data.
+     *
+     * If the Main Activity was waiting for a EDIT_STUDENT result, it will update the Student
+     * using the received data.
+     *
+     * @param requestCode The code that the caller Activity has made
+     * @param resultCode The code that the called Activity has sent as a result
+     * @param data All the data that the called Activity has sent as a result
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode){
@@ -92,6 +129,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This function is called when the menu is created.
+     * In this case, we filter the visibility of the menu items based on the amount of selected
+     * Students.
+     *
+     * (I know that a flag pattern could be easier, but I didn't know how to implement that pattern
+     * in this environment and I managed to implement it this way.)
+     *
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.maintoolbar, menu);
@@ -121,13 +169,29 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    /**
+     * This function hides the Floating Action Button
+     */
     public void hideButton(){mBtnCreate.hide();}
+
+    /**
+     * This function shows the Floating Action Button
+     */
     public void showButton(){mBtnCreate.show();}
 
+    /**
+     * This function refreshes the menu options in order to recreate them.
+     */
     public void updateMenu(){
         invalidateOptionsMenu();
     }
 
+    /**
+     * This function is called when the Edit Button is pressed.
+     *
+     * It gets the selected Student and calls for result the UserEdit Activity.
+     */
     public void editStudent(){
         Cursor cuStudent = DbAdapter.getInstance(MainActivity.this).fetchStudent(
                 mStudentListFragment.mItemsAdapter.getIdFromPosition(mStudentListFragment.mItemsAdapter.getSelectedItem()
@@ -143,6 +207,11 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intentEditStudent, EDIT_STUDENT);
     }
 
+    /**
+     * This function is called when the Delete Button is pressed.
+     *
+     * It gets all the selected Students and iterate the list deleting all of them.
+     */
     public void deleteStudents(){
         for (Integer student:mStudentListFragment.mItemsAdapter.getSelectedItems()){
             DbAdapter.getInstance(MainActivity.this).deleteStudent(mStudentListFragment.mItemsAdapter.getIdFromPosition(student));
@@ -150,6 +219,10 @@ public class MainActivity extends AppCompatActivity {
         refresh();
     }
 
+    /**
+     * This function refreshes the list. Recreating the list Student row items, clearing the
+     * selected Student and refreshing the Menu Options.
+     */
     private void refresh(){
         mStudentListFragment.fillData();
         mStudentListFragment.mItemsAdapter.clearSelection();
